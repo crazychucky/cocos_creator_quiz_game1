de4800d93a7b0a2054fd699b88041c3f16a50514
yuanlixiao <yuanlixiao@live.cn>
2021-10-07 16:01:30 +0800

    init

---
+++ assets/TestButton.ts
@@ -0,0 +1,54 @@
+
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+/**
+ * Predefined variables
+ * Name = TestButton
+ * DateTime = Sat Oct 02 2021 16:16:30 GMT+0800 (中国标准时间)
+ * Author = crazychucky
+ * FileBasename = TestButton.ts
+ * FileBasenameNoExtension = TestButton
+ * URL = db://assets/TestButton.ts
+ * ManualUrl = https://docs.cocos.com/creator/3.3/manual/en/
+ *
+ */
+ 
+@ccclass('TestButton')
+export class TestButton extends Component {
+    // 声明 Player 属性
+    @property({ type: Node })
+    private btn = null;
+    // cc.Node btn;
+    // [1]
+    // dummy = '';
+
+    // [2]
+    // @property
+    // serializableDummy = 0;
+
+    start () {
+        // [3]
+    }
+
+    // update (deltaTime: number) {
+    //     // [4]
+    // }
+
+    onClick(){
+      console.log("click");
+      this.btn.x = 50;
+      // this.btn.active = false;
+    }
+}
+
+/**
+ * [1] Class member could be defined like this.
+ * [2] Use `property` decorator if your want the member to be serializable.
+ * [3] Your initialization goes here.
+ * [4] Your update function goes here.
+ *
+ * Learn more about scripting: https://docs.cocos.com/creator/3.3/manual/en/scripting/
+ * Learn more about CCClass: https://docs.cocos.com/creator/3.3/manual/en/scripting/ccclass.html
+ * Learn more about life-cycle callbacks: https://docs.cocos.com/creator/3.3/manual/en/scripting/life-cycle-callbacks.html
+ */
---
+++ temp/programming/packer-driver/targets/preview/mods/cce/internal/x/prerequisite-imports.js
@@ -0,0 +1,9 @@
+System.register(["__unresolved_0"], function (_export, _context) {
+  "use strict";
+
+  return {
+    setters: [function (_unresolved_) {}],
+    execute: function () {}
+  };
+});
+//# sourceMappingURL=prerequisite-imports.js.map
